bcac0d069a7624bed43a40592d05266fa1723edc
matheus55391 <matheus.felipe55391@gmail.com>
2025-09-03 05:21:11 -0300

    feat: update @nestjs/config to v4.0.2, add prisma dependency, and create env_example file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from '../users/users.module'; // <-- importar o módulo que fornece UsersService
 
 @Module({
   imports: [
     ConfigModule,
+    UsersModule,
     JwtModule.registerAsync({
       global: true,
       imports: [ConfigModule],
